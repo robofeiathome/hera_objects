1d9eb4692ba2d53280ad56cc6ce251f83d50a8c6
Khaled Hazime <hazimekhaled01@gmail.com>
2024-09-03 18:44:39 -0300

    Refactor object exclusion logic in Objects class

--- src/objects.py
+++ src/objects.py
@@ -77,9 +77,8 @@ class Objects:
                     trans, a = self.listener.lookupTransform("map", obj_id, rospy.Time(0))
                     print(f"map a = {trans}")
                     if trans[2] > request.lower_limit and trans[2] < request.upper_limit:
-                        print(obj_id[:-1])
                         print(request.exclude)
-                        if obj_id.strip('/').split('/')[1][:-1] in request.exclude:
+                        if obj_id.strip('/').split('/')[1] in request.exclude:
                             continue
 
                         value = math.sqrt(x**2 + y**2 + z**2)
